5248f003732d02f7254c6078a5efa11dcc52fcbe
soheil najafi <soheilnajafimail2@gmail.com>
2019-09-29 16:14:59 +0330

    create deleteUserLink url

--- src/app/modules/link/services/urls.ts
+++ src/app/modules/link/services/urls.ts
@@ -1,13 +1,15 @@
+import { environment } from "../../../../environments/environment";
 
-const serverAddress = 'http://localhost:3000'
+const serverAddress = environment.apiUrl;
 
 export const LinkUrls = {
-  create: serverAddress.concat('/link'),
-  info:(code: string) => serverAddress.concat('/info/', code ),
-  getUserLinks: serverAddress.concat('/link')
+  create: serverAddress.concat("/link"),
+  info: (code: string) => serverAddress.concat("/info/", code),
+  getUserLinks: serverAddress.concat("/link"),
+  deleteUserLink: (id: string) => serverAddress.concat("/link/", id)
 };
 
 export const LinkCategoriesUrl = {
-  save: serverAddress.concat('/link/category'),
-  get: serverAddress.concat('/link/category')
-}
+  save: serverAddress.concat("/link/category"),
+  get: serverAddress.concat("/link/category")
+};
