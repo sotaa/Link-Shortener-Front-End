53bbd7ed4c769f5850944596a9ee59a0120a8a22
soheil najafi <soheilnajafimail2@gmail.com>
2019-09-29 16:19:58 +0330

    create save function for password

--- src/app/modules/link/link-create/password-form/password-form.component.ts
+++ src/app/modules/link/link-create/password-form/password-form.component.ts
@@ -1,24 +1,32 @@
-import { ILink } from './../../../models/link.interface';
-import { Component, OnInit, Input } from '@angular/core';
+import { ILink } from "./../../../models/link.interface";
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-password-form',
-  templateUrl: './password-form.component.html',
-  styleUrls: ['./password-form.component.css']
+  selector: "app-password-form",
+  templateUrl: "./password-form.component.html",
+  styleUrls: ["./password-form.component.css"]
 })
 export class PasswordFormComponent implements OnInit {
-
   @Input() link: ILink;
   password: string;
+  minimumLength: number;
 
-  constructor() { }
+  constructor() {
+    this.minimumLength = 3;
+    this.password = "";
+  }
 
   ngOnInit() {
     this.password = this.link.password;
   }
 
   save() {
-    this.link.password = this.password;
-    alert('تغییرات پسورد اعمال شد');
+    const ck = document.getElementById("savePass") as HTMLInputElement;
+    if (ck.checked === true) {
+      this.link.password = this.password;
+      alert("تغییرات پسورد اعمال شد");
+    } else {
+      this.link.password = "";
+    }
   }
 }
