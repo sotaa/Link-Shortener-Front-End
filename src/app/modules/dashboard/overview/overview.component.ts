f085ada37248148fe5033b5c7f7426183e785f28
soheil najafi <soheilnajafimail2@gmail.com>
2019-09-29 16:07:45 +0330

    create getUserLink and deleteUserLink

--- src/app/modules/dashboard/overview/overview.component.ts
+++ src/app/modules/dashboard/overview/overview.component.ts
@@ -1,37 +1,57 @@
-import { ILink } from './../../models/link.interface';
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../../auth/services/auth.service';
-import { LinkService } from '../../link/services/link.service';
+import { Component, OnInit, Input } from "@angular/core";
+
+import { ILink } from "./../../models/link.interface";
+import { AuthService } from "../../auth/services/auth.service";
+import { LinkService } from "../../link/services/link.service";
 
 @Component({
-  selector: 'app-overview',
-  templateUrl: './overview.component.html',
-  styleUrls: ['./overview.component.css']
+  selector: "app-overview",
+  templateUrl: "./overview.component.html",
+  styleUrls: ["./overview.component.css"]
 })
 export class OverviewComponent implements OnInit {
-
   links: ILink[];
 
-  constructor(private authService: AuthService, private linkService: LinkService) { }
+  constructor(
+    private authService: AuthService,
+    private linkService: LinkService
+  ) {}
 
   ngOnInit() {
-  this.updateUserInfo();
+    this.updateUserInfo();
+    this.getUserLinks();
   }
 
+  async getUserLinks() {
+    await this.linkService
+      .getUserLinks()
+      .toPromise()
+      .then(links => {
+        this.links = links;
+      });
+  }
 
-  updateUserInfo(){
+  async deleteUserLink(id) {
+    await this.linkService
+      .deleteUserLink(id)
+      .toPromise()
+      .then(result => {
+        this.getUserLinks();
+      });
+  }
+
+  updateUserInfo() {
     // TODO: show spinner until get new data.
-    this.authService.getUserInfo().subscribe(user => {
-      if(user) {
-        this.authService.updateSavedUser(user,true);
+    this.authService.getUserInfo().subscribe(
+      user => {
+        if (user) {
+          this.authService.updateSavedUser(user, true);
+        }
+      },
+      err => {
+        // TODO: handle the error.
+        console.log(err);
       }
-    }, err => {
-      // TODO: handle the error.
-      console.log(err);
-    })
-  }
-
-  getUserLinks() {
-    this.linkService.getUserLinks().subscribe(links => this.links = links);
+    );
   }
 }
