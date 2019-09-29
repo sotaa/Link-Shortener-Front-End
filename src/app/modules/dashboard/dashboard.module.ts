c1cf5bf3906796426680104db3d0d930662e72c7
soheil najafi <soheilnajafimail2@gmail.com>
2019-09-29 15:20:43 +0330

    format text content

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -1,24 +1,29 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { OverviewComponent } from './overview/overview.component';
-import { DashboardRoutingModule } from './dashboard-routing/dashboard-routing.module';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { DashboardStartComponent } from './dashboard-start/dashboard-start.component';
-import { PremiumCtaComponent } from './overview/premium-cta/premium-cta.component';
-import { RemainingTimeComponent } from './overview/remaining-time/remaining-time.component';
-import { ExpiredComponent } from './overview/remaining-time/expired/expired.component';
-import { PlansComponent } from './plans/plans.component';
-import { PricePipe } from './pipes/price.pipe';
-import { LinksModule } from '../link/link.module';
-import { CreateLinkComponent } from './create-link/create-link.component';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { OverviewComponent } from "./overview/overview.component";
+import { DashboardRoutingModule } from "./dashboard-routing/dashboard-routing.module";
+import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { DashboardStartComponent } from "./dashboard-start/dashboard-start.component";
+import { PremiumCtaComponent } from "./overview/premium-cta/premium-cta.component";
+import { RemainingTimeComponent } from "./overview/remaining-time/remaining-time.component";
+import { ExpiredComponent } from "./overview/remaining-time/expired/expired.component";
+import { PlansComponent } from "./plans/plans.component";
+import { PricePipe } from "./pipes/price.pipe";
+import { LinksModule } from "../link/link.module";
+import { CreateLinkComponent } from "./create-link/create-link.component";
 
 @NgModule({
-  imports: [
-    CommonModule,
-    DashboardRoutingModule,
-    LinksModule
-  ],
-  declarations: [OverviewComponent, NavBarComponent, DashboardStartComponent,
-     PremiumCtaComponent, RemainingTimeComponent, ExpiredComponent, PlansComponent, PricePipe, CreateLinkComponent]
+  imports: [CommonModule, DashboardRoutingModule, LinksModule],
+  declarations: [
+    OverviewComponent,
+    NavBarComponent,
+    DashboardStartComponent,
+    PremiumCtaComponent,
+    RemainingTimeComponent,
+    ExpiredComponent,
+    PlansComponent,
+    PricePipe,
+    CreateLinkComponent
+  ]
 })
-export class DashboardModule { }
+export class DashboardModule {}
