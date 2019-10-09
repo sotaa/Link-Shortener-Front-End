0e768a803aac8cf15689b73f5b05718ffe8068d6
soheil najafi <soheilnajafimail2@gmail.com>
2019-10-10 00:36:01 +0330

    add route for login page

--- src/app/modules/app-routing/app-routing.module.ts
+++ src/app/modules/app-routing/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthComponent } from '../auth/auth/auth.component';
 
 const appRoutes: Routes = [
   {path: '', pathMatch: 'full' , loadChildren: '../public-view/pages/pages.module#PagesModule'},
-  {path: 'dashboard' , loadChildren: '../dashboard/dashboard.module#DashboardModule'}
+  { path: 'dashboard', loadChildren: '../dashboard/dashboard.module#DashboardModule' },
+  { path: 'login', component:AuthComponent },
 ];
 
 @NgModule({
