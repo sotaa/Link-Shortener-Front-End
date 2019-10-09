5350cb5f5d8c7fe6ce1b805c4232bc760d59be95
soheil najafi <soheilnajafimail2@gmail.com>
2019-10-09 18:28:20 +0330

    add update url and getUserLink url

--- src/app/modules/link/services/urls.ts
+++ src/app/modules/link/services/urls.ts
@@ -4,8 +4,10 @@ const serverAddress = environment.apiUrl;
 
 export const LinkUrls = {
   create: serverAddress.concat("/link"),
+  update: (id: string) => serverAddress.concat("/link/", id),
   info: (code: string) => serverAddress.concat("/info/", code),
   getUserLinks: serverAddress.concat("/link"),
+  getUserLink: (id: string) => serverAddress.concat("/link/", id),
   deleteUserLink: (id: string) => serverAddress.concat("/link/", id)
 };
 
