ebf8c536e6752638363596ea5c7d3133691423f5
soheil najafi <soheilnajafimail2@gmail.com>
2019-10-09 18:47:28 +0330

    refactor link-create.component to implement create link and edit link

--- src/app/modules/link/link-create/link-create.component.ts
+++ src/app/modules/link/link-create/link-create.component.ts
@@ -5,6 +5,7 @@ import { ILink, Link } from "../../models/link.interface";
 import { SystemMessagesService } from "../services/system-messages.service";
 import { PremiumFeature } from "../../models/premium-feature.model";
 import { environment } from "../../../../environments/environment";
+import { ActivatedRoute, Params, Router } from "@angular/router";
 
 @Component({
   selector: "app-link-create",
@@ -13,32 +14,60 @@ import { environment } from "../../../../environments/environment";
 })
 export class LinkCreateComponent extends PremiumFeature implements OnInit {
   link: ILink;
+  paramId: string;
+  editMode: boolean = false;
+  isLoading: boolean = true;
   messages;
   linkAddress: string;
 
   constructor(
     private linkService: LinkService,
-    msgService: SystemMessagesService
+    private msgService: SystemMessagesService,
+    private route: ActivatedRoute,
+    private router: Router
   ) {
     super();
-    this.messages = msgService.getMessages();
   }
 
   ngOnInit() {
-    // init link.
-    // create new link.
     this.link = new Link();
+    this.messages = this.msgService.getMessages()["generate-short-link"];
+    this.route.params.subscribe(async (params: Params) => {
+      if (params.id) {
+        this.paramId = params.id;
+        this.editMode = true;
+        this.messages = this.msgService.getMessages()["edit-short-link"];
+        this.link = await this.fetchLinkData(params.id);
+      }
+      this.isLoading = false;
+    });
   }
 
-  generate() {
-    this.linkService.generate(this.link).subscribe(link => {
-      this.link = link;
-      this.linkAddress = environment.apiUrl.concat(`/${link.shorten}`);
-    });
+  async fetchLinkData(id: string) {
+    return await this.linkService.getUserLink(id).toPromise();
+  }
+
+  submit() {
+    if (this.linkService.alertMessage) {
+      return alert(`${this.linkService.alertMessage}`);
+    } else {
+      if (this.editMode) {
+        this.linkService.updateLink(this.paramId, this.link).subscribe(() => {
+          this.router.navigate(["../dashboard"]);
+        });
+      } else {
+        this.linkService.generateLink(this.link).subscribe(link => {
+          this.link = link;
+          this.linkAddress = environment.apiUrl.concat(`/${link.shorten}`);
+        });
+      }
+    }
   }
 
   resetLink() {
     this.link = new Link();
+    this.linkAddress = "";
+    this.linkService.resetCustomLink();
   }
 
   copyToClipBoard() {
