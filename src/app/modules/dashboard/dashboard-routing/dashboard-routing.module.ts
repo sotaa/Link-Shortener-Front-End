bcc6a7263f420d693e1df3a3eb4a41b4f33e8e25
soheil najafi <soheilnajafimail2@gmail.com>
2019-10-10 10:40:07 +0330

    change route path

--- src/app/modules/dashboard/dashboard-routing/dashboard-routing.module.ts
+++ src/app/modules/dashboard/dashboard-routing/dashboard-routing.module.ts
@@ -1,27 +1,27 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { Routes, Router, RouterModule } from '@angular/router';
-import { OverviewComponent } from '../overview/overview.component';
-import { DashboardStartComponent } from '../dashboard-start/dashboard-start.component';
-import { PlansComponent } from '../plans/plans.component';
-import { CreateLinkComponent } from '../create-link/create-link.component';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { Routes, Router, RouterModule } from "@angular/router";
+import { OverviewComponent } from "../overview/overview.component";
+import { DashboardStartComponent } from "../dashboard-start/dashboard-start.component";
+import { PlansComponent } from "../plans/plans.component";
+import { CreateLinkComponent } from "../create-link/create-link.component";
 
 const routes: Routes = [
-  {path: '' , component: DashboardStartComponent , children: [
-    {path:'' , component: OverviewComponent},
-    {path: 'plans' , component: PlansComponent},
-    {path: 'init-link' , component: CreateLinkComponent},
-    {path: 'edit-link/:id' , component: CreateLinkComponent}
-  ]}
-]
-
+  {
+    path: "link",
+    component: DashboardStartComponent,
+    children: [
+      { path: "", component: OverviewComponent },
+      { path: "plans", component: PlansComponent },
+      { path: "create", component: CreateLinkComponent },
+      { path: "edit/:id", component: CreateLinkComponent }
+    ]
+  }
+];
 
 @NgModule({
-  imports: [
-    CommonModule,
-    RouterModule.forChild(routes)
-  ],
+  imports: [CommonModule, RouterModule.forChild(routes)],
   exports: [RouterModule],
   declarations: []
 })
-export class DashboardRoutingModule { }
+export class DashboardRoutingModule {}
