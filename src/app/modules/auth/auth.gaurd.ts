b7ec6b4204ffc75defbafc8a70350d7f77d24b58
soheil najafi <soheilnajafimail2@gmail.com>
2019-10-12 13:02:38 +0330

    create  authentication  guard

---
+++ src/app/modules/auth/auth.gaurd.ts
@@ -0,0 +1,25 @@
+import {
+  CanActivate,
+  RouterStateSnapshot,
+  ActivatedRouteSnapshot,
+  Router
+} from "@angular/router";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { AuthService } from "./services/auth.service";
+
+@Injectable({ providedIn: "root" })
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    router: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    if (this.authService.userInfo) {
+      return true;
+    } else {
+      this.router.navigate(["login"]);
+    }
+  }
+}
