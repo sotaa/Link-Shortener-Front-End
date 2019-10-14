915b5d0e3e6dc42e3cd7768a7b8460335ef2ee0e
soheil najafi <soheilnajafimail2@gmail.com>
2019-10-14 16:22:12 +0330

    remove 'private' from appendAuthToken in baseService

--- src/app/modules/base-items/base.service.ts
+++ src/app/modules/base-items/base.service.ts
@@ -1,26 +1,25 @@
-import { Http , Headers} from "@angular/http";
+import { Http, Headers } from "@angular/http";
 import { Injector } from "@angular/core";
 import { StorageService } from "./storage.service";
 
 export abstract class BaseService {
-
   protected headers: Headers;
   protected requestOptions: any;
   private http: Http;
   protected storageService: StorageService;
 
- constructor(injector: Injector ) {
+  constructor(injector: Injector) {
     this.headers = new Headers();
     this.http = injector.get(Http);
     this.storageService = injector.get(StorageService);
     this.headers.append("Content-Type", "application/json");
     this.appendAuthToken();
-    this.requestOptions = {headers: this.headers };
+    this.requestOptions = { headers: this.headers };
   }
 
   // send Http POST request.
-  protected post(url , body) {
-    return this.http.post(url , body , this.requestOptions);
+  protected post(url, body) {
+    return this.http.post(url, body, this.requestOptions);
   }
 
   // send Http GET request.
@@ -28,10 +27,9 @@ export abstract class BaseService {
     return this.http.get(url, this.requestOptions);
   }
 
-
   // send Http PUT request.
-  protected put(url , body) {
-    return this.http.put(url,body , this.requestOptions);
+  protected put(url, body) {
+    return this.http.put(url, body, this.requestOptions);
   }
 
   // send Http DELETE request.
@@ -41,12 +39,12 @@ export abstract class BaseService {
 
   // send HTTP HEAD request.
   protected head(url) {
-    return this.http.head(url , this.requestOptions);
+    return this.http.head(url, this.requestOptions);
   }
 
   // append token to header.
-  private appendAuthToken() {
-   const userInfo = this.storageService.getSavedUserInfo();
-   if(userInfo) this.headers.append('Authorization', userInfo.token);
+  appendAuthToken() {
+    const userInfo = this.storageService.getSavedUserInfo();
+    if (userInfo) this.headers.append("Authorization", userInfo.token);
   }
 }
