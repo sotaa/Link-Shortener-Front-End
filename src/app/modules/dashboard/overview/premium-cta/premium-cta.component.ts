c31329d74bbd6ea1b418a8771159a283db8eaa70
soheil najafi <soheilnajafimail2@gmail.com>
2019-10-15 16:34:00 +0330

    set default value for user

--- src/app/modules/dashboard/overview/premium-cta/premium-cta.component.ts
+++ src/app/modules/dashboard/overview/premium-cta/premium-cta.component.ts
@@ -1,21 +1,21 @@
-import { OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { IUser } from 'src/app/modules/models/user.interface';
-import { AuthService } from 'src/app/modules/auth/services/auth.service';
+import { OnDestroy } from "@angular/core";
+import { Subscription } from "rxjs";
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { IUser } from "src/app/modules/models/user.interface";
+import { AuthService } from "src/app/modules/auth/services/auth.service";
 
 @Component({
-  selector: 'app-premium-cta',
-  templateUrl: './premium-cta.component.html',
-  styleUrls: ['./premium-cta.component.css']
+  selector: "app-premium-cta",
+  templateUrl: "./premium-cta.component.html",
+  styleUrls: ["./premium-cta.component.css"]
 })
-export class PremiumCtaComponent implements OnInit , OnDestroy{
-
+export class PremiumCtaComponent implements OnInit, OnDestroy {
   user: IUser;
   userSubscription: Subscription;
-  constructor(private router: Router,private authService: AuthService) { }
-
+  constructor(private router: Router, private authService: AuthService) {
+    this.user = undefined;
+  }
 
   ngOnInit() {
     this.user = this.authService.getSavedUserInfo();
@@ -25,7 +25,7 @@ export class PremiumCtaComponent implements OnInit , OnDestroy{
   }
 
   goToPlans() {
-    this.router.navigate(['dashboard', 'plans'])
+    this.router.navigate(["dashboard", "plans"]);
   }
 
   ngOnDestroy() {
