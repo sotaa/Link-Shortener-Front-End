aaf2c0ed98a1652a42b4cd11203c60cdd307b7b2
soheil najafi <soheilnajafimail2@gmail.com>
2019-10-15 16:32:29 +0330

    set default value for user

--- src/app/modules/dashboard/overview/remaining-time/remaining-time.component.ts
+++ src/app/modules/dashboard/overview/remaining-time/remaining-time.component.ts
@@ -1,18 +1,19 @@
-import { Subscription } from 'rxjs';
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { IUser } from 'src/app/modules/models/user.interface';
-import { AuthService } from 'src/app/modules/auth/services/auth.service';
+import { Subscription } from "rxjs";
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { IUser } from "src/app/modules/models/user.interface";
+import { AuthService } from "src/app/modules/auth/services/auth.service";
 
 @Component({
-  selector: 'app-remaining-time',
-  templateUrl: './remaining-time.component.html',
-  styleUrls: ['./remaining-time.component.css']
+  selector: "app-remaining-time",
+  templateUrl: "./remaining-time.component.html",
+  styleUrls: ["./remaining-time.component.css"]
 })
-export class RemainingTimeComponent implements OnInit , OnDestroy{
-
+export class RemainingTimeComponent implements OnInit, OnDestroy {
   user: IUser;
   userSubscription: Subscription;
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService) {
+    this.user = undefined;
+  }
 
   ngOnInit() {
     this.user = this.authService.getSavedUserInfo();
