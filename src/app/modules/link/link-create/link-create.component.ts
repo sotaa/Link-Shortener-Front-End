9aa6268022d3a5cbba5c3731ceb4839e8f5c18a7
soheil najafi <soheilnajafimail2@gmail.com>
2019-10-22 21:09:56 +0330

    add regex for validate url and router.event for refetch some values when current route navigated and handle some error and add getUserInfo method and goPermium method

--- src/app/modules/link/link-create/link-create.component.ts
+++ src/app/modules/link/link-create/link-create.component.ts
@@ -1,32 +1,49 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 
 import { LinkService } from "../services/link.service";
 import { ILink, Link } from "../../models/link.interface";
 import { SystemMessagesService } from "../services/system-messages.service";
 import { PremiumFeature } from "../../models/premium-feature.model";
 import { environment } from "../../../../environments/environment";
-import { ActivatedRoute, Params, Router } from "@angular/router";
+import { ActivatedRoute, Params, Router, NavigationEnd } from "@angular/router";
+import { AuthService } from "../../auth/services/auth.service";
+import { Subscription } from "rxjs";
+import { IUser } from "../../models/user.interface";
 
 @Component({
   selector: "app-link-create",
   templateUrl: "./link-create.component.html",
   styleUrls: ["./link-create.component.css"]
 })
-export class LinkCreateComponent extends PremiumFeature implements OnInit {
+export class LinkCreateComponent extends PremiumFeature
+  implements OnInit, OnDestroy {
   link: ILink;
+  userInfo: IUser;
+  isExpired: boolean = true;
   paramId: string;
   editMode: boolean = false;
   isLoading: boolean = true;
   messages;
   linkAddress: string;
+  navigationSubscription: Subscription;
+  regexURL = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/;
+  disabled;
+  value;
 
   constructor(
     private linkService: LinkService,
+    private authService: AuthService,
     private msgService: SystemMessagesService,
     private route: ActivatedRoute,
     private router: Router
   ) {
     super();
+    this.userInfo = undefined;
+    this.navigationSubscription = this.router.events.subscribe((e: any) => {
+      if (e instanceof NavigationEnd) {
+        this.initSomeValuesAgain();
+      }
+    });
   }
 
   ngOnInit() {
@@ -38,8 +55,8 @@ export class LinkCreateComponent extends PremiumFeature implements OnInit {
         this.editMode = true;
         this.messages = this.msgService.getMessages()["edit-short-link"];
         this.link = await this.fetchLinkData(params.id);
+        this.isLoading = false;
       }
-      this.isLoading = false;
     });
   }
 
@@ -48,19 +65,42 @@ export class LinkCreateComponent extends PremiumFeature implements OnInit {
   }
 
   submit() {
+    //check adrress existence
+    if (!this.link.address) {
+      return alert("لطفا ادرس سایت را وارد کنید!");
+    }
+    //check address url validate
+    const myURL = this.regexURL.exec(this.link.address);
+    if (myURL == null) return alert("لطفا ادرس صحیح را وارد کنید!");
+    //check childeren component validate like custom link
     if (this.linkService.alertMessage) {
       return alert(`${this.linkService.alertMessage}`);
-    } else {
-      if (this.editMode) {
-        this.linkService.updateLink(this.paramId, this.link).subscribe(() => {
-          this.router.navigate(["../dashboard/link"]);
-        });
-      } else {
-        this.linkService.generateLink(this.link).subscribe(link => {
+    }
+
+    if (this.editMode) {
+      this.linkService.updateLink(this.paramId, this.link).subscribe(
+        res => {
+          if (res._id) this.router.navigate(["../dashboard/link"]);
+        },
+        error => {
+          if (error.status === 400)
+            return alert("لطفا ادرس صحیح را وارد کنید!");
+        }
+      );
+    }
+    if (!this.editMode) {
+      this.linkService.generateLink(this.link).subscribe(
+        link => {
           this.link = link;
           this.linkAddress = environment.apiUrl.concat(`/${link.shorten}`);
-        });
-      }
+        },
+        error => {
+          if (error.status === 400) {
+            return alert("لطفا ادرس صحیح را وارد کنید!");
+          }
+          console.log(error);
+        }
+      );
     }
   }
 
@@ -82,5 +122,47 @@ export class LinkCreateComponent extends PremiumFeature implements OnInit {
     document.execCommand("copy");
     document.body.removeChild(el);
     alert("کپی شد");
+  }
+
+  getUserInfo() {
+    this.authService.getUserInfo().subscribe(user => {
+      if (user) {
+        this.userInfo = user;
+      }
+      if (this.userInfo.remainingDays == 0 || null || undefined) {
+        this.isExpired = true;
+      } else {
+        this.isExpired = false;
+      }
+      if (!this.editMode) {
+        this.isLoading = false;
+      }
+    });
+  }
+
+  initSomeValuesAgain() {
+    this.isLoading = true;
+    const userLocalStorage = this.authService.getSavedUserInfo();
+    if (userLocalStorage) {
+      this.getUserInfo();
+    } else {
+      this.userInfo = undefined;
+      this.isExpired = true;
+      this.isLoading = false;
+    }
+  }
+
+  goPermium() {
+    const userLocalStorage = this.authService.getSavedUserInfo();
+    if (!userLocalStorage) {
+      this.router.navigate(["/login"]);
+    }
+    this.router.navigate(["/dashboard/link"]);
+  }
+
+  ngOnDestroy() {
+    if (this.navigationSubscription) {
+      this.navigationSubscription.unsubscribe();
+    }
   }
 }
