fb75eb4542e1e6eb32c9caf35c6bcf1048b90cab
soheil najafi <soheilnajafimail2@gmail.com>
2019-10-22 20:13:56 +0330

    add onSameUrlNavigation in routerModule for reload some valus in component  when that component is current

--- src/app/modules/app-routing/app-routing.module.ts
+++ src/app/modules/app-routing/app-routing.module.ts
@@ -1,22 +1,27 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { Routes, RouterModule } from '@angular/router';
-import { AuthComponent } from '../auth/auth/auth.component';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { Routes, RouterModule } from "@angular/router";
+import { AuthComponent } from "../auth/auth/auth.component";
 
 const appRoutes: Routes = [
-  {path: '', pathMatch: 'full' , loadChildren: '../public-view/pages/pages.module#PagesModule'},
-  { path: 'dashboard', loadChildren: '../dashboard/dashboard.module#DashboardModule' },
-  { path: 'login', component:AuthComponent },
+  {
+    path: "",
+    pathMatch: "full",
+    loadChildren: "../public-view/pages/pages.module#PagesModule"
+  },
+  {
+    path: "dashboard",
+    loadChildren: "../dashboard/dashboard.module#DashboardModule"
+  },
+  { path: "login", component: AuthComponent }
 ];
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes, { onSameUrlNavigation: "reload" })
   ],
-  exports: [
-    RouterModule
-  ],
+  exports: [RouterModule],
   declarations: []
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
