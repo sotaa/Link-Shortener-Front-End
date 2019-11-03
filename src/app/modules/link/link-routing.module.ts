a8352124d9a64a1fb4bdb102a5c93dd5b91ec131
soheil najafi <soheilnajafimail2@gmail.com>
2019-11-03 20:53:57 +0330

    add linkpassword component route

--- src/app/modules/link/link-routing.module.ts
+++ src/app/modules/link/link-routing.module.ts
@@ -1,13 +1,15 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { LinkInfoComponent } from './link-info/link-info.component';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { LinkInfoComponent } from "./link-info/link-info.component";
+import { LinkPasswordComponent } from "./link-password/link-password.component";
 
 const routes: Routes = [
-  {path: ':code/info' , component: LinkInfoComponent}
+  { path: ":code/info", component: LinkInfoComponent },
+  { path: ":code/password", component: LinkPasswordComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class LinkRoutingModule { }
+export class LinkRoutingModule {}
