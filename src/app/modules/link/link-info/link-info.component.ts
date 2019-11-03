f34016963fb08dd720f6f4c5ca1050d66f4cd07b
soheil najafi <soheilnajafimail2@gmail.com>
2019-11-03 20:33:44 +0330

    add condition for links with password

--- src/app/modules/link/link-info/link-info.component.ts
+++ src/app/modules/link/link-info/link-info.component.ts
@@ -1,6 +1,12 @@
 import { LinkService } from "./../services/link.service";
-import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  OnDestroy
+} from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
 import { Subscription } from "rxjs";
 import { LinkInfo } from "../../models/link-info.interface";
 import * as PersianDate from "persian-date";
@@ -11,7 +17,7 @@ import { environment } from "../../../../environments/environment";
   templateUrl: "./link-info.component.html",
   styleUrls: ["./link-info.component.css"]
 })
-export class LinkInfoComponent implements OnInit {
+export class LinkInfoComponent implements OnInit, OnDestroy {
   data: LinkInfo;
   userIsExpired;
   notFound = false;
@@ -21,28 +27,45 @@ export class LinkInfoComponent implements OnInit {
 
   constructor(
     private linkService: LinkService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router
   ) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    let code;
     this.routeSubscription = this.route.params.subscribe(params => {
-      const code = params["code"];
-      if (code)
-        this.linkService.getInfo(code).subscribe(data => {
-          this.userIsExpired = data.userIsExpired;
-          this.data = data;
-          this.notFound = !this.data;
-          this.getWindowUrl();
-          this.createDate = new PersianDate(data.linkInfo.createDateFa)
-            .toLocale("fa")
-            .format();
-        });
-      if (!this.data) this.notFound = true;
+      code = params["code"];
+      if (code) {
+        this.linkService
+          .getInfo(code)
+          .toPromise()
+          .then(data => {
+            this.userIsExpired = data.userIsExpired;
+            this.data = data;
+            this.notFound = !this.data;
+            this.getWindowUrl();
+            this.createDate = new PersianDate(data.linkInfo.createDateFa)
+              .toLocale("fa")
+              .format();
+          })
+          .catch(err => {
+            if (err.status == 401) this.router.navigate([`${code}/password/`]);
+            if (err.status == 400) {
+              alert("رمز عبور صحیح نمی باشد!");
+              this.router.navigate([`${code}/password/`]);
+            }
+          });
+      }
+      if (!this.data) return (this.notFound = true);
     });
   }
 
   private getWindowUrl() {
     const host = environment.apiUrl;
     this.shortenLink = host.concat("/", this.data.linkInfo.shorten);
+  }
+
+  ngOnDestroy() {
+    this.routeSubscription.unsubscribe();
   }
 }
