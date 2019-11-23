8ec596e3f85dbd1f2bbf980c93dcd1c16ded0e6f
soheil najafi <soheilnajafimail2@gmail.com>
2019-11-23 13:36:25 +0330

    uncomment getusercategory method

--- src/app/modules/link/services/category.service.ts
+++ src/app/modules/link/services/category.service.ts
@@ -1,5 +1,5 @@
 import { ICategory } from "./../../models/category.interface";
-import { Injectable, Injector } from "@angular/core";
+import { Injectable, Injector, EventEmitter, Output } from "@angular/core";
 import { BaseService } from "../../base-items/base.service";
 import { LinkCategoriesUrl } from "./urls";
 import { Response } from "@angular/http";
@@ -12,16 +12,14 @@ export class CategoryService extends BaseService {
   constructor(injector: Injector) {
     super(injector);
   }
-
   save(data: ICategory[]) {
     return this.post(LinkCategoriesUrl.save, data);
   }
 
   getUserCategories() {
-    return this.get(LinkCategoriesUrl.get)
-      .pipe
-      // map<Response, ICategory[]>(res => res.json())
-      ();
+    return this.get(LinkCategoriesUrl.get).pipe(
+      map<Response, ICategory[]>(res => res.json())
+    );
   }
 
   removeAuthTokenFromHeader() {
