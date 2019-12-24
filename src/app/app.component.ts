d27b8d6bd6038ec8025efdd64a3a1a176100e193
Mahdi Valizadeh <m.valizadeh@live.com>
2019-12-24 15:01:05 +0800

    Google tag manager added codes to index.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        /**
+         * Will emit pageview event for google analytics on production environment.
+         */
+        if (environment.production) {
+          (<any>window).ga('set', 'page', event.urlAfterRedirects);
+          (<any>window).ga('send', 'pageview');
+        }
+      }
+    });
+  }
+
   title = '20800 کوتاه کننده لینک';
 }
