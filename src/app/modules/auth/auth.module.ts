c1915feb79ae3f67c531b0f8dc82f3bfa30432df
soheil najafi <soheilnajafimail2@gmail.com>
2020-01-09 11:53:15 +0330

    add rulles and auth routing

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,13 +1,20 @@
-import { FormsModule } from '@angular/forms';
+import { FormsModule } from "@angular/forms";
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { LoginComponent } from "./login/login.component";
 import { RegisterComponent } from "./register/register.component";
-import { AuthComponent } from './auth/auth.component';
+import { AuthComponent } from "./auth/auth.component";
+import { RulesComponent } from "./register/rules/rules.component";
+import { AuthRoutingModule } from "./auth-routing.module";
 
 @NgModule({
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, AuthRoutingModule, FormsModule],
   exports: [LoginComponent, RegisterComponent, AuthComponent],
-  declarations: [LoginComponent, RegisterComponent, AuthComponent]
+  declarations: [
+    LoginComponent,
+    RegisterComponent,
+    AuthComponent,
+    RulesComponent
+  ]
 })
 export class AuthModule {}
